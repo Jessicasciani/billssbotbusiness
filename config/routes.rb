85182d8517bdcb0b8e531fbac5f159a19cdea4de
artmai <arthurmaisonnier@gmail.com>
2019-04-01 20:38:51 +0400

    aded privacy

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :downloads
+  get 'privacy', to: 'pages#privacy', as: :privacy
 end
